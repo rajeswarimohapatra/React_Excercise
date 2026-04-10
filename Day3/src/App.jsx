518100a615e0df94a8cefe582fc57f764359f2f2
Rajeswari Mohapatra <rajeswari.mohapatra@iserveu.co.in>
2026-04-10 09:49:21 +0530

    Day 3 excercise

--- Day3/src/App.jsx
+++ Day3/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import './App.css'
 import StatusCard from './StatusCard'
 import Header from './Header'
@@ -10,25 +9,28 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      <button className="btn">Click Me</button>
+    <div className="app">
+      <div className="container">
+
+        
+        <button className="btn">Click Me</button>
+
+        <div className="card">
+          <StatusCard type="success" />
+          <StatusCard type="error" />
+        </div>
+
+        <Header />
+        <div className="content">Content</div>
+        <Footer />
+        <br />
+        <br />
+
+       
+        <Navbar />
+
+      </div>
     </div>
-    <br />
-    <br />
-    <StatusCard type="success" />
-    <StatusCard type="error" />
-    <br />
-    <br />
-    <div>
-      <Header />
-      <h1>Content</h1>
-      <Footer />
-    </div>
-    <br />
-    <br />
-    <Navbar />
-    </>
   )
 }
 
